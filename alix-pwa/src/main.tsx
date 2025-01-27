888a77995d6d2065aa84fead37dfe1bd382488c1
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2025-01-27 18:49:27 +0100

    install pwa

--- alix-pwa/src/main.tsx
+++ alix-pwa/src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register('/service-worker.ts');
+      console.log('Service Worker registered successfully:', registration);
+    } catch (error) {
+      console.log('Service Worker registration failed:', error);
+    }
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
